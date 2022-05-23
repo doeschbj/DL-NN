da97857e664a53cfe4f36ade7809e325c9c1e4c0
Björn Döschl <bjoern-doesch@gmx.de>
2022-05-23 21:22:49 +0200

    hi

---
+++ Simple/model.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import numpy as np
+
+
+class Model:
+	def __init__(self):
+		self.layers = 1
+		self.neurons = 50
+		print("Init")
+
+	def forward():
+		pass
+
+	def backwar():
+		pass
+
+
+
+
+if __name__=="__main__":
+	model = Model()
